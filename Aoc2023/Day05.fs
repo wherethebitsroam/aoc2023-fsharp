f67141b371d731b5d5e0bc6f54f9c1f9e91a2bdb
Jeff Williams <jeffw@wherethebitsroam.com>
2023-12-06 13:54:21 +0100

    Cleanup day 5. Still ugly

--- Aoc2023/Day05.fs
+++ Aoc2023/Day05.fs
@@ -1,41 +1,30 @@
 ﻿module Aoc2023.Day05
 
-open System.Collections.Generic
 open FParsec
 
 type Mapping = {Src: int64; Dst: int64; Length: int64}
 
 module Mapping =
-    let srcEnd (m: Mapping) = m.Src + m.Length - 1L
-    
     let matchesSrc (x: int64) (m: Mapping) = x >= m.Src && x < m.Src + m.Length
     let matchesDst (x: int64) (m: Mapping) = x >= m.Dst && x < m.Dst + m.Length
     let mapSrc (x: int64) (m: Mapping) = x - m.Src + m.Dst
-    
-    let diff (m1: Mapping option) (m2: Mapping option) =
-        match (m1,m2) with
-        | Some m1, Some m2 -> m1.Dst - m1.Src + m2.Dst - m2.Src
-        | None, Some m | Some m, None -> m.Dst - m.Src
-        | _ -> failwith "wtf"
+    let inc (m: Mapping) = m.Dst - m.Src
         
-    let getSource (m1: Mapping option) (m2: Mapping option) (start: int64) =
-        match (m1,m2) with
-        | Some m, _ -> start - m.Dst + m.Src
-        | None, Some m -> start
-        | _ -> failwith "wtf"
-    
     let merge (ms1: Mapping list) (ms2: Mapping list) =
+        let map ((start, stop): int64 * int64) =
+            let m1 = ms1 |> List.filter (matchesDst start) |> List.tryHead
+            let m2 = ms2 |> List.filter (matchesSrc start) |> List.tryHead
+            let src = start + (m1 |> Option.map (fun m -> m.Src - m.Dst) |> Option.defaultValue 0)
+            let incr = [m1;m2] |> List.choose id |> List.map inc |> List.sum
+            { Src = src; Length = stop-start; Dst = src + incr }
+            
         let ms1Limits = ms1 |> List.fold (fun ls m -> ls @ [m.Dst; m.Dst + m.Length]) List.empty
         let ms2Limits = ms2 |> List.fold (fun ls m -> ls @ [m.Src; m.Src + m.Length]) List.empty
         ms1Limits @ ms2Limits
         |> List.sort
         |> List.pairwise
         |> List.filter (fun (a,b) -> a <> b)
-        |> List.map (fun (start,stop) -> (start, stop-start, ms1 |> List.filter (matchesDst start) |> List.tryHead, ms2 |> List.filter (matchesSrc start) |> List.tryHead))
-        |> List.map (fun (start,len,m1,m2) ->
-            let src = getSource m1 m2 start
-            { Src = src; Length = len; Dst = src + (diff m1 m2)})
-    
+        |> List.map map
 
 module Parser =
     let intList = sepBy pint64 (pstring " ")
@@ -73,9 +62,7 @@ let rec inSeeds (x: int64) (seeds: int64 list) =
 
 let part2 (s: string) =
     let seeds, mappings = s |> Parser.parse
-    let map =
-        mappings
-        |> List.fold Mapping.merge List.empty
+    let map = mappings |> List.reduce Mapping.merge
     
     map
     |> List.filter (fun m -> inSeeds m.Src seeds)
