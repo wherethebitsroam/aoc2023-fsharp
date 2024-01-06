63691a95b434c13a11644a4d9cd12e2b67345df1
Jeff Williams <jeffw@wherethebitsroam.com>
2024-01-06 12:43:01 +0100

    Day25 improved

--- Aoc2023/Day25.fs
+++ Aoc2023/Day25.fs
@@ -30,38 +30,33 @@ let connections (ls: (string * string list) list) =
     ls
     |> List.collect (fun (n,cs) -> cs |> List.collect (fun c -> [(n,c);(c,n)]))
     |> List.groupBy fst
-    |> List.map (fun (n,cs) -> (n, List.map snd cs))
+    |> List.map (fun (n,cs) -> (n, List.map snd cs |> Set.ofList))
     |> Map.ofList
     
-let partition (cs: Map<string,string list>) =
-    // get the number of connections to vertices to the set that are not in the set
-    let links (p: Set<string>) =
-        p
-        |> Set.toList
-        |> List.collect (fun v -> cs[v] |> List.filter (fun d -> Set.contains d p |> not) |> List.map (fun d -> (v,d)))
-        |> List.length
-        
-    let rec split (p1: Set<string>) (p2: Set<string>) =
-        let l = links p1
-        printfn "%A: links: %d" p1 l
-        if l = 3 then
+let partition (cs: Map<string,Set<string>>) =
+    let rec split (p1: Set<string>) (p2: Set<string>) (links: Set<string * string>) =
+        if Set.count links = 3 then
             (p1,p2)
         else
-            // find the vertex in p2 with the least connections to p2 if added to p1
-            let toMove =
-                p2
+            // find the vertex in links with the least connections to p2
+            let n,ls =
+                links
                 |> Set.toList
-                |> List.map (fun v -> (v, Set.add v p1 |> links))
-                |> List.minBy snd
-                |> fst
+                |> List.map (fun (_,v) -> (v, cs[v] |> Set.intersect p2))
+                |> List.minBy (fun (_,d) -> Set.count d)
             
             // move that vertex from p2 to p1
-            split (Set.add toMove p1) (Set.remove toMove p2)
+            let links =
+                links
+                |> Set.filter (fun (_,d) -> d <> n)
+                |> Set.union (ls |> Set.map (fun l -> (n,l)))
+            split (Set.add n p1) (Set.remove n p2) links
         
-    let p1 = cs.Keys |> Seq.head |> Set.singleton
+    let p1 = cs.Keys |> Seq.head
     let p2 = cs.Keys |> Seq.skip 1 |> Set.ofSeq
+    let links = cs[p1] |> Set.map (fun c -> (p1,c))
     
-    split p1 p2
+    split (Set.singleton p1) p2 links
 
 let part1 (s: string) =
     let p1,p2 = s |> Parser.parse |> connections |> partition
